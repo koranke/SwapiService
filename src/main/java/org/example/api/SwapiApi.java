70a35294b0adfaec9e83a08757065f7f60936b88
koranke <gcaccia@outlook.com>
2024-07-10 15:19:21 -0700

    updated readme and added comment.

--- src/main/java/org/example/api/SwapiApi.java
+++ src/main/java/org/example/api/SwapiApi.java
@@ -15,6 +15,17 @@ public class SwapiApi<T> extends ApiBase<SwapiApi<T>> {
 	private final Type resultType;
 	private final Type resultListType;
 
+	/*
+	For the sake of Gson deserialization, we need to pass the type of the object we want to deserialize,
+	both for single objects and for lists of objects.
+
+	Here's an example of how to do this from the extending class.
+
+			super("planets",
+				new TypeToken<Result<Planet>>(){}.getType(),
+				new TypeToken<List<Result<Planet>>>(){}.getType());
+
+	 */
 	public SwapiApi(String endpoint, Type type, Type listType) {
 		baseUrl = Constants.BASE_SWAPI_URL + endpoint + "/";
 		resultType = type;
